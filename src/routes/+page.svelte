8414ea4e833f580a3ac7e6b32dfa0b159b2f0686
Gurnoor Singh <lejakewolf@protonmail.com>
2023-02-27 16:09:41 +0530

    design for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let range = Array(20);
+</script>
+
+<div class="navbar bg-base-100 flex justify-center">
+	<div class="title mt-3 ml-7">
+		<a href="/" class="btn btn-ghost normal-case text-xl text-sky-800 title">Makes360 Event</a>
+	</div>
+</div>
+<div class="divider " />
+
+<div class="m-6 flex flex-wrap image-full">
+	{#each range as _}
+		<!-- <div class="carousel-item">
+			<img class="w-72" src="/image.jpg" alt="test" />
+		</div> -->
+		<div class="card m-10 w-60 bg-base-100">
+			<figure><img class="" src="/image.jpg" alt="test" /></figure>
+			<!-- <div class="card-body"> -->
+			<button class="btn btn-primary border-radius-none">Download</button>
+			<!-- </div> -->
+		</div>
+	{/each}
+</div>
+
+<style>
+	.title {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
